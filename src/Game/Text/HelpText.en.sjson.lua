9408c16a2db81fe32b71443e286e351694a36669
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-19 19:05:56 +0000

    Updated IDs

--- src/Game/Text/HelpText.en.sjson.lua
+++ src/Game/Text/HelpText.en.sjson.lua
@@ -11,147 +11,147 @@ local order = {
 -- It's a bit too much even, as the "unread" marker overlaps the text
 local newData = {
   {
-    Id = "Song_Hades_MainTheme",
+    Id = "MusicMusicPlayerMainThemeMusicPlayer",
     DisplayName = "Hades - No Escape",
     Description = "A rousing theme created for the god of the dead."
   },
   {
-    Id = "Song_Hades_HadesTheme",
+    Id = "MusicMusicPlayerHadesThemeMusicPlayer",
     DisplayName = "Hades - Death and I",
     Description = "A brief and frankly unhappy piece Lord Hades enjoys."
   },
   {
-    Id = "Song_Hades_MusicExploration4",
+    Id = "MusicMusicPlayerMusicExploration4MusicPlayer",
     DisplayName = "Hades - The House of Hades",
     Description = "Understated theme permitted in the House of Hades."
   },
   {
-    Id = "Song_Hades_MusicHadesReset",
+    Id = "MusicMusicPlayerMusicHadesResetMusicPlayer",
     DisplayName = "Hades - Out of Tartarus",
     Description = "Were there a way out, this piece would be about it."
   },
   {
-    Id = "Song_Hades_MusicHadesReset2",
+    Id = "MusicMusicPlayerMusicHadesReset2MusicPlayer",
     DisplayName = "Hades - The Painful Way",
     Description = "About a quick, turbulent trip down the River Styx."
   },
   {
-    Id = "Song_Hades_MusicHadesReset3",
+    Id = "MusicMusicPlayerMusicHadesReset3MusicPlayer",
     DisplayName = "Hades - Mouth of Styx",
     Description = "The hideous gloom of Tartarus in musical form."
   },
   {
-    Id = "Song_Hades_MusicTartarus4",
+    Id = "MusicMusicPlayerMusicTartarus4MusicPlayer",
     DisplayName = "Hades - Scourge of the Furies",
     Description = "In tribute to the well-feared Erinyes."
   },
   {
-    Id = "Song_Hades_MusicAsphodel1",
+    Id = "MusicMusicPlayerMusicAsphodel1MusicPlayer",
     DisplayName = "Hades - Through Asphodel",
     Description = "About a once-lush meadows now awash in flame."
   },
   {
-    Id = "Song_Hades_MusicAsphodel2",
+    Id = "MusicMusicPlayerMusicAsphodel2MusicPlayer",
     DisplayName = "Hades - River of Flame",
     Description = "The feared River Phlegethon inspired this piece."
   },
   {
-    Id = "Song_Hades_MusicAsphodel3",
+    Id = "MusicMusicPlayerMusicAsphodel3MusicPlayer",
     DisplayName = "Hades - Field of Souls",
     Description = "For those who passed and now reside in Asphodel."
   },
   {
-    Id = "Song_Hades_MusicElysium1",
+    Id = "MusicMusicPlayerMusicElysium1MusicPlayer",
     DisplayName = "Hades - The King and the Bull",
     Description = "Dedicated to the King of Athens and the Minoan Bull."
   },
   {
-    Id = "Song_Hades_MusicElysium2",
+    Id = "MusicMusicPlayerMusicElysium2MusicPlayer",
     DisplayName = "Hades - The Exalted",
     Description = "Tribute to the mighty heroes of Elysium."
   },
   {
-    Id = "Song_Hades_MusicElysium3",
+    Id = "MusicMusicPlayerMusicElysium3MusicPlayer",
     DisplayName = "Hades - Rage of the Myrmidons",
     Description = "Commemorates a famous warrior tribe."
   },
   {
-    Id = "Song_Hades_MusicStyx1",
+    Id = "MusicMusicPlayerMusicStyx1MusicPlayer",
     DisplayName = "Hades - Gates of Hell",
     Description = "Dedicated to those trespassing in the Temple of Styx."
   },
   {
-    Id = "Song_Hades_BossFightMusic",
+    Id = "MusicMusicPlayerBossFightMusicMusicPlayer",
     DisplayName = "Hades - God of the Dead",
     Description = "Commissioned long ago in honor of Lord Hades."
   },
   {
-    Id = "Song_Hades_TheUnseenOnes",
+    Id = "MusicMusicPlayerTheUnseenOnesMusicPlayer",
     DisplayName = "Hades - The Unseen Ones",
     Description = "Banned for containing too many music notes per second."
   },
   {
-    Id = "Song_Hades_CharonShopTheme",
+    Id = "MusicMusicPlayerCharonShopThemeMusicPlayer",
     DisplayName = "Hades - Final Expense",
     Description = "Commissioned expressly for the Stygian Boatman."
   },
   {
-    Id = "Song_Hades_CharonFightTheme",
+    Id = "MusicMusicPlayerCharonFightThemeMusicPlayer",
     DisplayName = "Hades - Final Expense (Payback Mix)",
     Description = "Commissioned expressly for the Stygian Boatman."
   },
   {
-    Id = "Song_Hades_ChaosTheme",
+    Id = "MusicMusicPlayerChaosThemeMusicPlayer",
     DisplayName = "Hades - Primordial Chaos",
     Description = "Ode to the oft-forgotten originator of all things."
   },
   {
-    Id = "Song_Hades_ThanatosTheme",
+    Id = "MusicMusicPlayerThanatosThemeMusicPlayer",
     DisplayName = "Hades - Last Words",
     Description = "Heralds the arrival of Thanatos, incarnate of death."
   },
   {
-    Id = "Song_Hades_MusicExploration1",
+    Id = "MusicMusicPlayerMusicExploration1MusicPlayer",
     DisplayName = "Hades - Wretched Shades",
     Description = "In memory of lives snuffed out and sent to this realm."
   },
   {
-    Id = "Song_Hades_MusicExploration2",
+    Id = "MusicMusicPlayerMusicExploration2MusicPlayer",
     DisplayName = "Hades - The Bloodless",
     Description = "In testament to the wrathful souls serving Lord Hades."
   },
   {
-    Id = "Song_Hades_MusicExploration3",
+    Id = "MusicMusicPlayerMusicExploration3MusicPlayer",
     DisplayName = "Hades - From Olympus",
     Description = "Solemn piece reflecting the divide between realms."
   },
   {
-    Id = "Song_Hades_OrpheusSong1",
+    Id = "MusicMusicPlayerOrpheusSong1MusicPlayer",
     DisplayName = "Hades - Lament of Orpheus",
     Description = "The court-musician's regrets captured in song form."
   },
   {
-    Id = "Song_Hades_OrpheusSong2",
+    Id = "MusicMusicPlayerOrpheusSong2MusicPlayer",
     DisplayName = "Hades - Hymn to Zagreus",
     Description = "Ballad of the Prince of the Underworld's exploits."
   },
   {
-    Id = "Song_Hades_EurydiceSong1",
+    Id = "MusicMusicPlayerEurydiceSong1MusicPlayer",
     DisplayName = "Hades - Good Riddance",
     Description = "A rousing celebration of the afterlife."
   },
   {
-    Id = "Song_Hades_PersephoneTheme",
+    Id = "MusicMusicPlayerPersephoneThemeMusicPlayer",
     DisplayName = "Hades - On the Coast",
     Description = "Exotic piece uses instruments from the surface world."
   },
   {
-    Id = "Song_Hades_EndTheme",
+    Id = "MusicMusicPlayerEndThemeMusicPlayer",
     DisplayName = "Hades - In the Blood",
     Description = "Popular duet reflects on home and family."
   },
   {
-    Id = "Song_Hades_EurydiceSong1Solo",
+    Id = "MusicMusicPlayerEurydiceSong1SoloMusicPlayer",
     DisplayName = "Hades - Good Riddance (Eurydice Solo)",
     Description = "A rousing celebration of the afterlife, sung by the muse herself."
   },
--- src/Scripts/MusicPlayerData.lua
+++ src/Scripts/MusicPlayerData.lua
@@ -2,264 +2,264 @@
 ---@diagnostic disable
 
 local songNames = {
-	"Song_Hades_MainTheme",
-	"Song_Hades_HadesTheme",
-	"Song_Hades_MusicExploration4",
-	"Song_Hades_MusicHadesReset",
-	"Song_Hades_MusicHadesReset2",
-	"Song_Hades_MusicHadesReset3",
-	"Song_Hades_MusicTartarus4",
-	"Song_Hades_MusicAsphodel1",
-	"Song_Hades_MusicAsphodel2",
-	"Song_Hades_MusicAsphodel3",
-	"Song_Hades_MusicElysium1",
-	"Song_Hades_MusicElysium2",
-	"Song_Hades_MusicElysium3",
-	"Song_Hades_MusicStyx1",
-	"Song_Hades_BossFightMusic",
-	"Song_Hades_TheUnseenOnes",
-	"Song_Hades_CharonShopTheme",
-	"Song_Hades_CharonFightTheme",
-	"Song_Hades_ChaosTheme",
-	"Song_Hades_ThanatosTheme",
-	"Song_Hades_MusicExploration1",
-	"Song_Hades_MusicExploration2",
-	"Song_Hades_MusicExploration3",
-	"Song_Hades_OrpheusSong1",
-	"Song_Hades_OrpheusSong2",
-	"Song_Hades_EurydiceSong1",
-	"Song_Hades_PersephoneTheme",
-	"Song_Hades_EndTheme",
-	"Song_Hades_EurydiceSong1Solo",
+	"MusicMusicPlayerMainThemeMusicPlayer",
+	"MusicMusicPlayerHadesThemeMusicPlayer",
+	"MusicMusicPlayerMusicExploration4MusicPlayer",
+	"MusicMusicPlayerMusicHadesResetMusicPlayer",
+	"MusicMusicPlayerMusicHadesReset2MusicPlayer",
+	"MusicMusicPlayerMusicHadesReset3MusicPlayer",
+	"MusicMusicPlayerMusicTartarus4MusicPlayer",
+	"MusicMusicPlayerMusicAsphodel1MusicPlayer",
+	"MusicMusicPlayerMusicAsphodel2MusicPlayer",
+	"MusicMusicPlayerMusicAsphodel3MusicPlayer",
+	"MusicMusicPlayerMusicElysium1MusicPlayer",
+	"MusicMusicPlayerMusicElysium2MusicPlayer",
+	"MusicMusicPlayerMusicElysium3MusicPlayer",
+	"MusicMusicPlayerMusicStyx1MusicPlayer",
+	"MusicMusicPlayerBossFightMusicMusicPlayer",
+	"MusicMusicPlayerTheUnseenOnesMusicPlayer",
+	"MusicMusicPlayerCharonShopThemeMusicPlayer",
+	"MusicMusicPlayerCharonFightThemeMusicPlayer",
+	"MusicMusicPlayerChaosThemeMusicPlayer",
+	"MusicMusicPlayerThanatosThemeMusicPlayer",
+	"MusicMusicPlayerMusicExploration1MusicPlayer",
+	"MusicMusicPlayerMusicExploration2MusicPlayer",
+	"MusicMusicPlayerMusicExploration3MusicPlayer",
+	"MusicMusicPlayerOrpheusSong1MusicPlayer",
+	"MusicMusicPlayerOrpheusSong2MusicPlayer",
+	"MusicMusicPlayerEurydiceSong1MusicPlayer",
+	"MusicMusicPlayerPersephoneThemeMusicPlayer",
+	"MusicMusicPlayerEndThemeMusicPlayer",
+	"MusicMusicPlayerEurydiceSong1SoloMusicPlayer",
 }
 
 local songWorldUpgradeData = {
-	Song_Hades_MainTheme = {
-		Name = "Song_Hades_MainTheme",
+	MusicMusicPlayerMainThemeMusicPlayer = {
+		Name = "MusicMusicPlayerMainThemeMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{2c36bb06-113d-4c63-8c35-ed21f4eb94ae}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_HadesTheme = {
-		Name = "Song_Hades_HadesTheme",
+	MusicMusicPlayerHadesThemeMusicPlayer = {
+		Name = "MusicMusicPlayerHadesThemeMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{57fbe830-4207-4601-8ca1-cd69eebfa742}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicExploration4 = {
-		Name = "Song_Hades_MusicExploration4",
+	MusicMusicPlayerMusicExploration4MusicPlayer = {
+		Name = "MusicMusicPlayerMusicExploration4MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{c1f44b16-95c0-48a5-a2e7-3fa0916e08a0}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicHadesReset = {
-		Name = "Song_Hades_MusicHadesReset",
+	MusicMusicPlayerMusicHadesResetMusicPlayer = {
+		Name = "MusicMusicPlayerMusicHadesResetMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{f00ef76c-1c18-47d2-9bf3-b883bc15e8ef}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicHadesReset2 = {
-		Name = "Song_Hades_MusicHadesReset2",
+	MusicMusicPlayerMusicHadesReset2MusicPlayer = {
+		Name = "MusicMusicPlayerMusicHadesReset2MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{2393a5bc-9662-4002-913e-83b575d1c056}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicHadesReset3 = {
-		Name = "Song_Hades_MusicHadesReset3",
+	MusicMusicPlayerMusicHadesReset3MusicPlayer = {
+		Name = "MusicMusicPlayerMusicHadesReset3MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{7fffc963-0826-4f11-81c5-a3eed6818bb9}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicTartarus4 = {
-		Name = "Song_Hades_MusicTartarus4",
+	MusicMusicPlayerMusicTartarus4MusicPlayer = {
+		Name = "MusicMusicPlayerMusicTartarus4MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{eb9bfcbd-fdae-4f68-9ed2-531c6168dd6d}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicAsphodel1 = {
-		Name = "Song_Hades_MusicAsphodel1",
+	MusicMusicPlayerMusicAsphodel1MusicPlayer = {
+		Name = "MusicMusicPlayerMusicAsphodel1MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{29359d10-547e-4a94-84c1-b31e4fcdeb92}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicAsphodel2 = {
-		Name = "Song_Hades_MusicAsphodel2",
+	MusicMusicPlayerMusicAsphodel2MusicPlayer = {
+		Name = "MusicMusicPlayerMusicAsphodel2MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{c872078c-f057-4882-a7e1-e0bcacde1414}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicAsphodel3 = {
-		Name = "Song_Hades_MusicAsphodel3",
+	MusicMusicPlayerMusicAsphodel3MusicPlayer = {
+		Name = "MusicMusicPlayerMusicAsphodel3MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{b0533fd9-9980-4fc6-b0a7-23813f2cda1c}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicElysium1 = {
-		Name = "Song_Hades_MusicElysium1",
+	MusicMusicPlayerMusicElysium1MusicPlayer = {
+		Name = "MusicMusicPlayerMusicElysium1MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{957caeee-56d0-4ae1-a872-81e01e6fea88}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicElysium2 = {
-		Name = "Song_Hades_MusicElysium2",
+	MusicMusicPlayerMusicElysium2MusicPlayer = {
+		Name = "MusicMusicPlayerMusicElysium2MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{e71ca020-6a35-448c-8855-8098b6fe5e58}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicElysium3 = {
-		Name = "Song_Hades_MusicElysium3",
+	MusicMusicPlayerMusicElysium3MusicPlayer = {
+		Name = "MusicMusicPlayerMusicElysium3MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{e025788e-6814-4bfe-9b73-5be5d3465572}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicStyx1 = {
-		Name = "Song_Hades_MusicStyx1",
+	MusicMusicPlayerMusicStyx1MusicPlayer = {
+		Name = "MusicMusicPlayerMusicStyx1MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{aff7e6d5-24ca-4aec-b209-be29d626a427}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_BossFightMusic = {
-		Name = "Song_Hades_BossFightMusic",
+	MusicMusicPlayerBossFightMusicMusicPlayer = {
+		Name = "MusicMusicPlayerBossFightMusicMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{fc71b797-75db-43af-8cc0-f50fdacb5dbc}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_TheUnseenOnes = {
-		Name = "Song_Hades_TheUnseenOnes",
+	MusicMusicPlayerTheUnseenOnesMusicPlayer = {
+		Name = "MusicMusicPlayerTheUnseenOnesMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{50ebf4d6-4c76-416f-9c32-33787f99f27e}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_CharonShopTheme = {
-		Name = "Song_Hades_CharonShopTheme",
+	MusicMusicPlayerCharonShopThemeMusicPlayer = {
+		Name = "MusicMusicPlayerCharonShopThemeMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{1d2d987b-853b-4a65-aa2d-a3e8c7e0e99b}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_CharonFightTheme = {
-		Name = "Song_Hades_CharonFightTheme",
+	MusicMusicPlayerCharonFightThemeMusicPlayer = {
+		Name = "MusicMusicPlayerCharonFightThemeMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{75c830f2-e8b3-4a59-949b-be106e2ff1e6}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_ChaosTheme = {
-		Name = "Song_Hades_ChaosTheme",
+	MusicMusicPlayerChaosThemeMusicPlayer = {
+		Name = "MusicMusicPlayerChaosThemeMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{2c8f3fd6-6c6c-4146-a750-20be93cd8ca8}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_ThanatosTheme = {
-		Name = "Song_Hades_ThanatosTheme",
+	MusicMusicPlayerThanatosThemeMusicPlayer = {
+		Name = "MusicMusicPlayerThanatosThemeMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{1e20eb07-28cb-4b8d-9d01-a9c199c79118}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicExploration1 = {
-		Name = "Song_Hades_MusicExploration1",
+	MusicMusicPlayerMusicExploration1MusicPlayer = {
+		Name = "MusicMusicPlayerMusicExploration1MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{eb3e0bed-2912-4c2e-af4e-3860fae954e9}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicExploration2 = {
-		Name = "Song_Hades_MusicExploration2",
+	MusicMusicPlayerMusicExploration2MusicPlayer = {
+		Name = "MusicMusicPlayerMusicExploration2MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{b4191f68-8b2a-4e15-9c94-5d1a5c52bc85}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_MusicExploration3 = {
-		Name = "Song_Hades_MusicExploration3",
+	MusicMusicPlayerMusicExploration3MusicPlayer = {
+		Name = "MusicMusicPlayerMusicExploration3MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{d74e85b5-da0d-4758-a3d7-2d468b531bf3}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_OrpheusSong1 = {
-		Name = "Song_Hades_OrpheusSong1",
+	MusicMusicPlayerOrpheusSong1MusicPlayer = {
+		Name = "MusicMusicPlayerOrpheusSong1MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{c0cdae19-b74e-4da2-87a8-20ad7d5f6e1d}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_OrpheusSong2 = {
-		Name = "Song_Hades_OrpheusSong2",
+	MusicMusicPlayerOrpheusSong2MusicPlayer = {
+		Name = "MusicMusicPlayerOrpheusSong2MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{f960ff2a-1407-4136-86bc-ca5d3f633290}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_EurydiceSong1 = {
-		Name = "Song_Hades_EurydiceSong1",
+	MusicMusicPlayerEurydiceSong1MusicPlayer = {
+		Name = "MusicMusicPlayerEurydiceSong1MusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{82cccfdf-8990-4ece-b3c3-52e549b91d5e}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_EurydiceSong1Solo = {
-		Name = "Song_Hades_EurydiceSong1Solo",
+	MusicMusicPlayerEurydiceSong1SoloMusicPlayer = {
+		Name = "MusicMusicPlayerEurydiceSong1SoloMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{2888a0d0-33b5-454d-b596-220cf5922de1}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_PersephoneTheme = {
-		Name = "Song_Hades_PersephoneTheme",
+	MusicMusicPlayerPersephoneThemeMusicPlayer = {
+		Name = "MusicMusicPlayerPersephoneThemeMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{9155f02e-dbb8-4f3b-93c6-bac757bb17f5}",
 		Cost = {
 			CosmeticsPoints = 0,
 		},
 	},
-	Song_Hades_EndTheme = {
-		Name = "Song_Hades_EndTheme",
+	MusicMusicPlayerEndThemeMusicPlayer = {
+		Name = "MusicMusicPlayerEndThemeMusicPlayer",
 		InheritFrom = { "DefaultSongItem" },
 		TrackName = "{05c22860-d3f1-4f90-b5d7-cfa24a587f61}",
 		Cost = {
