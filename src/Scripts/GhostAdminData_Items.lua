4f0dcf98cf73293ce19832f1667af9237c846e87
NikkelM <57323886+NikkelM@users.noreply.github.com>
2024-11-23 12:40:46 +0000

    Insert the new WorldUpgrade directly after the MusicPlayer upgrade

--- src/Scripts/GhostAdminData_Items.lua
+++ src/Scripts/GhostAdminData_Items.lua
@@ -1,11 +1,12 @@
-local newGhostAdminPurchaseData = {
-	"WorldUpgradeMusicPlayerModsNikkelMUnlockHadesMusic"
-}
-
+-- Adds the new upgrade to the list of upgrades in the cauldron
 for _, ghostAdminPurchaseType in ipairs(game.ScreenData.GhostAdmin.ItemCategories) do
 	if ghostAdminPurchaseType.Name == "WorldUpgradeScreen_Critical" then
-		for _, newUpgrade in ipairs(newGhostAdminPurchaseData) do
-			table.insert(ghostAdminPurchaseType, newUpgrade)
+		-- Insert the new upgrade after the music player upgrade
+		for i, upgrade in ipairs(ghostAdminPurchaseType) do
+			if upgrade == "WorldUpgradeMusicPlayer" then
+				table.insert(ghostAdminPurchaseType, i + 1, "WorldUpgradeMusicPlayerModsNikkelMUnlockHadesMusic")
+				break
+			end
 		end
 	end
 end
